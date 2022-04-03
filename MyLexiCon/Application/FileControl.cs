0f3253ed200a36be4b7028b031f49e36e50fbec6
Joud Asfari <asfarijoud@ecs.vuw.ac.nz>
2022-04-03 20:18:55 +1200

    Creating file in createFile().

--- MyLexiCon/Application/FileControl.cs
+++ MyLexiCon/Application/FileControl.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace LexiconApplication
 {
@@ -13,7 +15,23 @@ namespace LexiconApplication
 		 * - Language font (optional)
 		 * Also adds columns for word, pronunciation, romanisation, and definition.
 		 */
-		public static void createFile() { }
+		public static void createFile(String fileName, String path, String langName,
+						String langVersion = "1.0", String langFont = null) {
+			try
+            {
+				using (FileStream stream = File.Create(path + "/" + fileName + ".csv"))
+                {
+					String header = langName + "," + "version " + langVersion+"\n";
+					String columns = "Word,Pronunciation,Romanisation,Definition \n";
+					byte[] encodedHeader = new UTF8Encoding(header + columns);
+					stream.Write(encodedHeader, 0, encodedHeader.Length);
+                }
+
+			} catch (Exception e)
+            {
+				Console.WriteLine(e.StackTrace); // printing call stack for testing purposes
+            }
+		}
 
 		/**
 		 * Import a csv file into the project.
@@ -24,12 +42,16 @@ namespace LexiconApplication
 		/**
 		 * Save changes into file.
 		 */
-		public static void saveFile() { }
+		public static bool saveFile() {
+			return true;
+		}
 
 		/**
 		 * Save changes into the file name defined by the user.
 		 */
-		public static void saveFileAs() { }
+		public static bool saveFileAs() {
+			return true;
+		}
 
 		private static bool canChangeFile() {
 			return false;	
